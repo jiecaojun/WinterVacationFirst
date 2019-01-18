dbe3ff1ef4e8f7acc3237680ac31e06b5e7af06a
jiecaojun <m15011312976@163.com>
2019-01-18 17:27:39 +0800

    aaa

--- app/src/main/java/com/example/administrator/wintervacationfirst/MainActivity.java
+++ app/src/main/java/com/example/administrator/wintervacationfirst/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.administrator.wintervacationfirst;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn1=(Button)findViewById(R.id.btn1);
+        final TextView textView=(TextView)findViewById(R.id.textView);
+        Switch mswitch=(Switch)findViewById(R.id.switch1);
+
+
+        mswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    textView.setText("有点快乐");
+                }else {
+                    textView.setText("不快乐");
+                }
+                Log.e("MainActivity", textView.toString());
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("现在不快乐");
+                String s=textView.toString();
+                Log.e("MainActivity","hahahah");
+            }
+        });
     }
 }
